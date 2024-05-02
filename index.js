2bdb15d7a3a30ec0058bc412d28cf4d97c15fecf
Simone Avancini <simone.avancini@studenti.unitn.it>
2024-05-02 12:51:00 +0200

    Configurazione mongoose+dotenv

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const app = require('./app/app.js');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const port = process.env.PORT || 8080;
 
 /**
  * Configure mongoose
  */
-// mongoose.Promise = global.Promise;
-// app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-//     .then ( () => {
-//         console.log("Connected to Database");
+app.locals.db = mongoose.connect(process.env.DB_URL, { serverApi: { version: '1', strict: true, deprecationErrors: true } })
+    .then ( () => {
+        console.log("Connected to Database");
 
-//         app.listen(port, () => {
-//             console.log(`Server listening on port ${port}`);
-//         });
-//     });
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    });
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
