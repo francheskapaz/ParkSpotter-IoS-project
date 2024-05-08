b36b24397397163b127a739b77adf9cd11fbe93f
Francheska <fp.contrerasdelgado@studenti.unitn.it>
2024-05-08 14:26:36 +0200

    Confirmation menssage

--- index.js
+++ index.js
@@ -1,29 +1,89 @@
 const express = require('express') 
 const app = express()
-const utenteregistratoJSON = require('./utenteregistrato.json')
+const parcheggi = require('./parcheggi.json')
+const cors = require('cors') 
+const {validateParcheggio, validatePartialParcheggio} = require('./scheme/parcheggio.js')
+app.use(express.json())
 
-const PORT = process.env.PORT ?? 8080
+app.use(cors({
+  origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:63394',
+        `http://localhost:${PORT}`
+      ]
 
-app.get('/utente/utenteregistrato', (req, res) => {
-    res.json(utenteregistratoJSON)
+      if (ACCEPTED_ORIGINS.includes(origin)){
+          return callback(null, true)
+      }
+      if (!origin){
+          return callback(null, true)
+      }
+      return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
+app.get('/parcheggi', (req, res) => {
+  res.json(parcheggi)
 })
 
-app.post('/utente', (req, res) => {
-    let body = ''
-    req.on('data', chunk => {
-      body += chunk.toString()
-    })
-    req.on('end', () => {
-      const data = JSON.parse(body)
-      data.timestamp = Date.now()
-      res.status(201).json(data) 
-    })
-  })
+app.get('/parcheggi/:id', (req, res) => {
+  const { id } = req.params
+  const foundParcheggio = parcheggi.find(parcheggio => parcheggio.id === id)
+  if (foundParcheggio) {
+    return res.json (foundParcheggio)
+  } else {
+    res.status(404).json({ message: 'Parcheggio not found'})
+  }
+})
 
-  app.use((req, res) => {
-    res.status(404).send('<h1>404</h1>')
-  })
+app.post('/parcheggi', (req, res) => {
+  const result = validateParcheggio(req.body)
+  if (result.error){
+    res.status(400).json( { error: JSON.parse(result.error.message)})
+  }
+  // in basi di dati
+  const newParcheggio = {
+    ...result.data
+  }
+  parcheggi.push(newParcheggio) //non è REST, da sistemare dopo
+  res.status(201).json(newParcheggio)
+})
 
-  app.listen(PORT, () => {
-    console.log('server listening on port http://localhost:8080')
-  })
+app.patch('/parcheggi/:id', (req, res) => {
+  const result = validatePartialParcheggio(req.body)
+  if (!result.success){
+    return res.status(400).json({error: JSON.parse(result.error.message)})
+  }
+  const { id } = req.params
+  const parcheggioIndex = parcheggi.findIndex(parcheggio => parcheggio.id === id)
+
+  if (parcheggioIndex === -1){
+    return res.status(404).json({ message: 'Parcheggio not found'})
+  }
+
+  const updateParcheggio = {
+    ...parcheggi[parcheggioIndex],
+    ...result.data
+  }
+  parcheggi[parcheggioIndex] = updateParcheggio
+  return res.json(updateParcheggio)
+})
+
+app.delete('/parcheggi/:id', (req, res) => {
+  const { id } = req.params
+  const parcheggioIndex = parcheggi.findIndex(parcheggio => parcheggio.id === id)
+
+  if (parcheggioIndex === -1){
+    return res.status(404).json({ message: 'Parcheggio not found'})
+  }
+
+  parcheggi.splice(parcheggioIndex, 1)
+  return res.json({ message: 'parcheggio deleted'})
+})
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
