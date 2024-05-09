ca7e6d0e775bc8c4c66e52f120166c4004d5f6ae
Simone Avancini <simone.avancini@studenti.unitn.it>
2024-05-09 18:48:39 +0200

    UI mappa

--- app/app.js
+++ app/app.js
@@ -3,4 +3,11 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+
+/**
+ * Serve static files for the front-end
+ */
+app.use('/', express.static('static'));
+
+
 module.exports = app;
