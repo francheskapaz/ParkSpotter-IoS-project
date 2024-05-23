7f7ff478bab3a3e08e7ece708bad76e66a8fee52
Francheska <fp.contrerasdelgado@studenti.unitn.it>
2024-05-23 16:41:38 +0200

    Chamges

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ const { corsMiddleware } = require('../middlewares/cors.js');
 
 const parcheggiRouter = require('./parcheggi.js');
 
+const feedbackRouter = require('./feedback.js');
+
 
 
 app.use(json()) // Adding the json middleware to parse incoming request bodies as JSON
@@ -19,5 +21,8 @@ app.use(express.json());
 
 //app.use(corsMiddleware()) // Using the cors middleware to enable Cross-Origin Resource Sharing (CORS)
 app.use('/api/v1/parkings', parcheggiRouter) // Mounting the parcheggiRouter to handle requests for the '/apiParcheggi/parcheggi' endpoint
+
+
+app.use('/api/v1/feedback', feedbackRouter) // Mounting the feedbackRouter to handle requests for the '/apiParcheggi/feedback' endpoint
 
 module.exports =  app;
---
+++ app/feedback.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const Feedback = require('../models/feedback.js');
+
+router.post('/', async (req, res) => {
+
+    
+});
