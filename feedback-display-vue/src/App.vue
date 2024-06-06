2fadaacec8f3d5dd193b7949ff8086e32d3742fe
Francheska <fp.contrerasdelgado@studenti.unitn.it>
2024-06-06 20:08:25 +0200

    Add UI Feedback

---
+++ feedback-display-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <FeedbackListAndForm />
+  </div>
+</template>
+
+<script>
+import  FeedbackListAndForm  from './components/FeedbackListAndForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FeedbackListAndForm
+  }
+};
+</script>
+
+<style>
+
+</style>
