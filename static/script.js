1b516db8e39af97557539b4e59eaf5c98619c36f
Bonadux <120679197+Bonadux@users.noreply.github.com>
2024-06-11 18:39:48 +0200

    update frontend

--- static/script.js
+++ static/script.js
@@ -1,26 +1,12 @@
-document.getElementById('reservation-form').addEventListener('submit', function(event) {
+document.getElementById('feeForm').addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const parkingId = document.getElementById('parking-id').value;
-    const timeStart = document.getElementById('time-start').value;
-    const timeEnd = document.getElementById('time-end').value;
-
-    fetch(`http://localhost:3000/${parkingId}/reservations`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ timeStart, timeEnd }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            document.getElementById('message').textContent = data.error;
-        } else {
-            document.getElementById('message').textContent = data.message;
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+    const parkingName = document.getElementById('parkingName').value;
+    fetch(`http://localhost:8080/api/parking/${parkingName}/totalFee`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('totalFee').textContent = 'La somma totale delle tariffe è: ' + data.totalFee;
+        })
+        .catch(error => {
+            console.error('Errore:', error);
+        });
 });
