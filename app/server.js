e650fd1b45b597739aad17b1d56c2c7758410a99
Bonadux <120679197+Bonadux@users.noreply.github.com>
2024-06-11 16:31:33 +0200

    API

--- app/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const parkingRouter = require('./app.js'); // Importa il modulo di routing
-
-app.use(express.json()); // Middleware per il parsing del corpo delle richieste JSON
-
-app.use('/api/parking', parkingRouter); 
-
-module.exports = app; // Esporta l'app Express
